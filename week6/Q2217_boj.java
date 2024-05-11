e9e9d85c052f5284a01c1af735ce0353dd315fd9
Moonjunwoong <myc0611@naver.com>
2024-05-11 11:20:41 +0900

    feat : Q2217_boj

--- week6/Q2217_boj.java
+++ week6/Q2217_boj.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collection;
 
 public class Q2217_boj {
     public static void main(String[] args) throws IOException {
