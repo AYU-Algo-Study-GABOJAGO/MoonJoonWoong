7a36347c82156e0ec0b2bc644b5e5c5009466e16
Moonjunwoong <myc0611@naver.com>
2024-05-17 16:56:57 +0900

    feat : Q19598_boj

---
+++ week6/Q19598_boj.java
@@ -0,0 +1,35 @@
+package week6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q19598_boj {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] room = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            room[i][0] = Integer.parseInt(st.nextToken());
+            room[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(room, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+
+        int cnt = 1;
+        int end = room[0][1];
+
+        for (int i = 1; i < N; i++) {
+            if(room[i][1] >= end) {
+                end = room[i][1];
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
