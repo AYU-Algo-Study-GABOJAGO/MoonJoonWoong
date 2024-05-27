c5309babbc4709a4a38962aa07d9b147160b34eb
Moonjunwoong <myc0611@naver.com>
2024-05-27 17:02:27 +0900

    feat : Q11659_boj

---
+++ week8/Q11659_boj.java
@@ -0,0 +1,33 @@
+package week8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q11659_boj {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=N;i++){
+            arr[i] = arr[i-1]+Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1;i<=M;i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            sb.append((arr[end]-arr[start-1]) + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
